3fe2cd94e23601d084e4ca698e0995f69ea4311c
Kushaal <43180237+kushaalrana@users.noreply.github.com>
2023-08-05 16:56:16 -0400

    Time: 38 ms (10%), Space: 13.5 MB (48.47%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -6,12 +6,14 @@ public:
         int m = graph[0].size();
         queue<int> q;
         q.push(start);
-      
+        color[start]=0; //use the 2 colors 1 and 0
         while(!q.empty()){
             int node = q.front();
             q.pop();
-
-            for(auto it: graph[node]){
+            cout<<endl;
+                                         // 0    1      2       
+            for(auto it: graph[node]){  //[123] [02]  [013]
+                cout<<it<<" ";
                 if(color[it]==-1){
                     color[it]=!color[node];
                     q.push(it);
@@ -21,7 +23,7 @@ public:
                 }
             }
         }
-return true;
+        return true;
     }
 
     bool isBipartite(vector<vector<int>>& graph) {
